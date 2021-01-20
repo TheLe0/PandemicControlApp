f82e27a0349fbdf61db6cbf8c4281103d764acd7
Leonardo Bertele Tosin <leob.tosin@hotmail.com>
2021-01-20 18:36:30 -0300

    Add SharedPreferences for the counting

--- application/app/src/main/java/br/com/leotosin/pandemiccontrol/MainActivity.kt
+++ application/app/src/main/java/br/com/leotosin/pandemiccontrol/MainActivity.kt
@@ -1,17 +1,25 @@
 package br.com.leotosin.pandemiccontrol
 
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import android.widget.TextView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
+import br.com.leotosin.pandemiccontrol.setting.Configuration
+
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val counter : TextView = findViewById(R.id.countingField)
+        val counting = this.getStoredCounting()
+        counter.text = counting
 
         val settings :FloatingActionButton = findViewById(R.id.settingsButton)
         settings.setOnClickListener{
@@ -23,18 +31,48 @@ class MainActivity : AppCompatActivity() {
     fun increaseOne(view: View)
     {
         val counter : TextView = findViewById(R.id.countingField)
-        counter.text = (counter.text.toString().toInt() + 1).toString()
+        val counting = (counter.text.toString().toInt() + 1).toString()
+        counter.text = counting
+        this.updateCounting(counting)
     }
 
     fun decreaseOne(view: View)
     {
         val counter : TextView = findViewById(R.id.countingField)
-        counter.text = (counter.text.toString().toInt() - 1).toString()
+        val counting = (counter.text.toString().toInt() - 1).toString()
+        counter.text = counting
+        this.updateCounting(counting)
     }
 
     fun reset(view: View)
     {
         val counter : TextView = findViewById(R.id.countingField)
-        counter.text = getString(R.string.startCounting)
+        val counting = getString(R.string.startCounting)
+        counter.text = counting
+        this.updateCounting(counting)
     }
+
+    private fun updateCounting(counting :String)
+    {
+        val preferences : SharedPreferences = getSharedPreferences(
+                Configuration.CONFIG_FILE,
+                0
+        )
+        val editor :SharedPreferences.Editor = preferences.edit()
+        editor.putString("counting", counting)
+        editor.commit()
+    }
+
+    private fun getStoredCounting() :String?
+    {
+        val preferences : SharedPreferences = getSharedPreferences(
+                Configuration.CONFIG_FILE,
+                0
+        )
+        return preferences.getString(
+                "counting",
+                getString(R.string.startCounting)
+        )
+    }
+
 }
---
+++ application/app/src/main/java/br/com/leotosin/pandemiccontrol/setting/Configuration.kt
@@ -0,0 +1,7 @@
+package br.com.leotosin.pandemiccontrol.setting
+
+object Configuration
+{
+
+    const val CONFIG_FILE = "PandemicPreferences"
+}
